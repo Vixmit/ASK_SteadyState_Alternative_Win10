c4c3eb6984940c8069e70552b408a14e504f2af3
Vixmit <37965548+Vixmit@users.noreply.github.com>
2019-03-19 17:45:54 +0100

    Add files via upload

--- ASK_SteadyState_Alternative_Win10/App.xaml.cs
+++ ASK_SteadyState_Alternative_Win10/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ASK_SteadyState_Alternative_Win10
+namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for App.xaml
